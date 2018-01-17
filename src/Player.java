0473d57380319459ee44b2b70e7ba36cae3f03bb
Mihir Singhal <mihir.a.singhal@gmail.com>
2018-01-16 22:23:32 -0800

    formatting changes

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@
 // See xxx for the javadocs.
 
 import bc.*;
+
 import java.util.*;
 
 public class Player {
@@ -64,20 +65,20 @@ public class Player {
 
 		totalRocketCost = bc.bcUnitTypeBlueprintCost(UnitType.Rocket); // TODO multiply this by the number of connected components on Mars
 
-		for(int i = 0; i < earthWidth; i++) {
-			for(int j = 0; j < earthHeight; j++) {
+		for (int i = 0; i < earthWidth; i++) {
+			for (int j = 0; j < earthHeight; j++) {
 				MapLocation curLoc = new MapLocation(Planet.Earth, i, j);
 				earthKarb[i][j] = earthMap.initialKarboniteAt(curLoc);
 			}
 		}
 
-		if(gc.planet() == Planet.Earth) {
+		if (gc.planet() == Planet.Earth) {
 			calculateBFS();
 			determinePaths();
 		}
 
-		for(long round = 1; round <= 1000; round++) {
-			if(asteroidPattern.hasAsteroid(round)) {
+		for (long round = 1; round <= 1000; round++) {
+			if (asteroidPattern.hasAsteroid(round)) {
 				AsteroidStrike strike = asteroidPattern.asteroid(round);
 				MapLocation curLoc = strike.getLocation();
 				int x = curLoc.getX(), y = curLoc.getY();
@@ -91,15 +92,15 @@ public class Player {
 		while (true) {
 
 			researchInfo = gc.researchInfo();
-			if(gc.round() % 100 == 0) {
+			if (gc.round() % 100 == 0) {
 				System.out.println("Round: " + gc.round());
 				System.out.println("Karbonite: " + gc.karbonite());
 				System.out.println("Research");
 				System.out.println("--------");
-				for(UnitType type : UnitType.values()) {
+				for (UnitType type : UnitType.values()) {
 					System.out.println(type + " at level " + researchInfo.getLevel(type));
 				}
-				if(researchInfo.hasNextInQueue())
+				if (researchInfo.hasNextInQueue())
 					System.out.println("Next up: " + researchInfo.nextInQueue());
 				// System.out.println(researchInfo.toJson());
 			}
@@ -121,8 +122,8 @@ public class Player {
 		dist = new int[earthWidth][earthHeight];
 		trace = new Direction[earthWidth][earthHeight];
 		go = new ArrayList[earthWidth][earthHeight];
-		for(int i = 0; i < earthWidth; i++) {
-			for(int j = 0; j < earthHeight; j++) {
+		for (int i = 0; i < earthWidth; i++) {
+			for (int j = 0; j < earthHeight; j++) {
 				dist[i][j] = -1;
 				go[i][j] = new ArrayList<Direction>();
 			}
@@ -136,32 +137,31 @@ public class Player {
 			Unit unit = units.get(i);
 			// assert unit.unitType() == UnitType.Worker;
 			MapLocation cur = unit.location().mapLocation();
-			if(bfs.offer(cur)) bfsSize++;
+			if (bfs.offer(cur)) bfsSize++;
 			dist[cur.getX()][cur.getY()] = 0;
 		}
 
-		while(bfsSize > 0) {
+		while (bfsSize > 0) {
 			MapLocation cur = bfs.poll();
-			if(cur == null) break; // just in case
+			if (cur == null) break; // just in case
 			bfsSize--;
 			int cx = cur.getX(), cy = cur.getY();
-			for(Direction dir : directions) {
+			for (Direction dir : directions) {
 				MapLocation nxt = cur.add(dir);
-				if(earthMap.onMap(nxt) == false) continue;
-				if(earthMap.isPassableTerrainAt(nxt) == 0) continue; // should be boolean?
+				if (earthMap.onMap(nxt) == false) continue;
+				if (earthMap.isPassableTerrainAt(nxt) == 0) continue; // should be boolean?
 				int x = nxt.getX(), y = nxt.getY();
-				if(dist[x][y] != -1) continue;
+				if (dist[x][y] != -1) continue;
 				trace[x][y] = bc.bcDirectionOpposite(dir);
-				if(wanted(x, y)) {
-					if(bfs.offer(nxt)) bfsSize++;
+				if (wanted(x, y)) {
+					if (bfs.offer(nxt)) bfsSize++;
 					dist[x][y] = dist[cx][cy];
-				}
-				else {
-					if(bfs2.offer(nxt)) bfsSize2++;
+				} else {
+					if (bfs2.offer(nxt)) bfsSize2++;
 					dist[x][y] = dist[cx][cy] + 1;
 				}
 			}
-			if(bfsSize == 0) {
+			if (bfsSize == 0) {
 				Queue<MapLocation> tmp = bfs;
 				bfs = bfs2;
 				bfs2 = tmp;
@@ -174,8 +174,8 @@ public class Player {
 		// Distances
 		System.out.println("Distances");
 		System.out.println("---------");
-		for(int j = earthHeight - 1; j >= 0; j--) {
-			for(int i = 0; i < earthWidth; i++) {
+		for (int j = earthHeight - 1; j >= 0; j--) {
+			for (int i = 0; i < earthWidth; i++) {
 				System.out.print(dist[i][j] + " ");
 			}
 			System.out.println();
@@ -184,17 +184,17 @@ public class Player {
 		// Visualizing the Directions
 		System.out.println("Directions");
 		System.out.println("----------");
-		for(int j = earthHeight - 1; j >= 0; j--) {
-			for(int i = 0; i < earthWidth; i++) {
+		for (int j = earthHeight - 1; j >= 0; j--) {
+			for (int i = 0; i < earthWidth; i++) {
 				Direction d = trace[i][j];
-				if(d == Direction.North) System.out.print("| ");
-				else if(d == Direction.Northwest) System.out.print("\\ ");
-				else if(d == Direction.Northeast) System.out.print("/ ");
-				else if(d == Direction.East) System.out.print("- ");
-				else if(d == Direction.Southeast) System.out.print("\\ ");
-				else if(d == Direction.Southwest) System.out.print("/ ");
-				else if(d == Direction.South) System.out.print("| ");
-				else if(d == Direction.West) System.out.print("- ");
+				if (d == Direction.North) System.out.print("| ");
+				else if (d == Direction.Northwest) System.out.print("\\ ");
+				else if (d == Direction.Northeast) System.out.print("/ ");
+				else if (d == Direction.East) System.out.print("- ");
+				else if (d == Direction.Southeast) System.out.print("\\ ");
+				else if (d == Direction.Southwest) System.out.print("/ ");
+				else if (d == Direction.South) System.out.print("| ");
+				else if (d == Direction.West) System.out.print("- ");
 				else System.out.print("  ");
 			}
 			System.out.println();
@@ -205,36 +205,36 @@ public class Player {
 		boolean vis[][] = new boolean[earthWidth][earthHeight];
 		final int maxDist = 100; // let's get everything within...
 		ArrayList<Integer> near[] = new ArrayList[maxDist];
-		for(int i = 0; i < maxDist; i++) {
+		for (int i = 0; i < maxDist; i++) {
 			near[i] = new ArrayList<Integer>();
 		}
-		for(int i = 0; i < earthWidth; i++) {
-			for(int j = 0; j < earthHeight; j++) {
-				if(wanted(i, j) && dist[i][j] < maxDist) {
-					near[dist[i][j]].add(1000 * i + j);
+		for (int i = 0; i < earthWidth; i++) {
+			for (int j = 0; j < earthHeight; j++) {
+				if (wanted(i, j) && dist[i][j] < maxDist) {
+					near[-dist[i][j]].add(1000 * i + j);
 				}
 			}
 		}
 		// for(int i = 0; i < earthWidth; i++) {
-			// for(int j = 0; j < earthHeight; j++) {
-		for(int d = 0; d < maxDist; d++) {
-			for(int pos : near[d]) {
+		// for(int j = 0; j < earthHeight; j++) {
+		for (int d = 0; d < maxDist; d++) {
+			for (int pos : near[d]) {
 				int i = pos / 1000, j = pos % 1000;
 				// condition for if we want to collect from this cell
 				// for now we will get any cell that has karbonite
 				// if(wanted(i, j)) {
-					MapLocation cur = new MapLocation(Planet.Earth, i, j);
-					while(true) {
-						int x = cur.getX(), y = cur.getY();
-						if(vis[x][y]) break;
-						vis[x][y] = true;
-						if(trace[x][y] == null) break;
-						MapLocation par = cur.add(trace[x][y]);
-						int px = par.getX(), py = par.getY();
-						go[px][py].add(bc.bcDirectionOpposite(trace[x][y]));
-						System.out.println("(" + px + ", " + py + ") --> (" + x + ", " + y + ")");
-						cur = par;
-					}
+				MapLocation cur = new MapLocation(Planet.Earth, i, j);
+				while (true) {
+					int x = cur.getX(), y = cur.getY();
+					if (vis[x][y]) break;
+					vis[x][y] = true;
+					if (trace[x][y] == null) break;
+					MapLocation par = cur.add(trace[x][y]);
+					int px = par.getX(), py = par.getY();
+					go[px][py].add(bc.bcDirectionOpposite(trace[x][y]));
+					System.out.println("(" + px + ", " + py + ") --> (" + x + ", " + y + ")");
+					cur = par;
+				}
 				// }
 			}
 		}
@@ -248,9 +248,9 @@ public class Player {
 		int id = unit.id();
 		MapLocation loc = unit.location().mapLocation();
 		int x = loc.getX(), y = loc.getY();
-		if(go[x][y].size() == 0) return;
-		for(Direction dir : go[x][y]) {
-			if(gc.canReplicate(id, dir)) {
+		if (go[x][y].size() == 0) return;
+		for (Direction dir : go[x][y]) {
+			if (gc.canReplicate(id, dir)) {
 				System.out.println("unit " + id + " at x = " + x + " y = " + y);
 				System.out.println("there are " + go[x][y].size() + " branches here!");
 				System.out.println("replicated in dir " + dir);
@@ -386,7 +386,7 @@ public class Player {
 		MapLocation loc = unit.location().mapLocation();
 		long maxk = 0;
 		Direction maxdir = null;
-		for (Direction dir: directions) {
+		for (Direction dir : directions) {
 			if (gc.canSenseLocation(loc.add(dir)) && (gc.karboniteAt(loc.add(dir)) > maxk)) {
 				maxk = gc.karboniteAt(loc.add(dir));
 				maxdir = dir;
